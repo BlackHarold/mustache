2dfa2dd2e2bb17381b70b0c85408967a35edf9a3
blackharold <info@teh-dozor.ru>
2018-05-21 02:41:38 +0300

    added UserService

--- controller/MainController.java
+++ controller/MainController.java
@@ -1,8 +1,10 @@
 package home.blackharold.sweater.controller;
 
 import home.blackharold.sweater.domain.Message;
+import home.blackharold.sweater.domain.User;
 import home.blackharold.sweater.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,28 +26,38 @@ public class MainController {
     public String main(Map<String, Object> model) {
         Iterable<Message> messages = messageRepository.findAll();
         model.put("messages", messages);
+
         return "main";
     }
     
     @PostMapping ("/main")
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
-        Message message = new Message(text, tag);
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag,
+            Map<String, Object> model) {
+        
+        Message message = new Message(text, tag , user);
         messageRepository.save(message);
         
         Iterable<Message> messages = messageRepository.findAll();
         model.put("messages", messages);
+        
         return "main";
     }
-    
-    @PostMapping ("filter")
+
+    @PostMapping("filter")
     public String filter(@RequestParam String filter, Map<String, Object> model) {
         Iterable<Message> messages;
+
         if (filter != null && !filter.isEmpty()) {
             messages = messageRepository.findByTag(filter);
         } else {
             messages = messageRepository.findAll();
         }
+
         model.put("messages", messages);
+
         return "main";
     }
 }
--- domain/User.java
+++ domain/User.java
@@ -1,54 +1,89 @@
 package home.blackharold.sweater.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Set;
 
 @Entity
-public class User {
-    
+@Table (name = "usr")
+public class User implements UserDetails {
     @Id
     @GeneratedValue (strategy = GenerationType.AUTO)
-    private Integer id;
-    private String name;
-    private String email;
-    private String pass;
+    private Long id;
+    private String username;
+    private String password;
+    private boolean active;
     
-    public String getPass() {
-        return pass;
-    }
+    @ElementCollection (targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable (name = "user_role", joinColumns = @JoinColumn (name = "user_id"))
+    @Enumerated (EnumType.STRING)
+    private Set<Role> roles;
     
-    public void setPass(String pass) {
-        this.pass = pass;
-    }
-    
-    public User(){}
-    
-    public User(String name, String email) {
-    }
-    
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
     
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
     
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
     
-    public void setName(String name) {
-        this.name = name;
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
     }
     
-    public String getEmail() {
-        return email;
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
     }
     
-    public void setEmail(String email) {
-        this.email = email;
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+    
+    @Override
+    public boolean isEnabled() {
+        return isActive();
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public boolean isActive() {
+        return active;
+    }
+    
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+    
+    public Set<Role> getRoles() {
+        return roles;
+    }
+    
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
     }
 }
